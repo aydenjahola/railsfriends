8ec461211c7f925031a29923e103039dfd457ac0
Ayden Jahola <mm5621333@gmail.com>
2021-12-02 20:05:58 +0000

    initial commit

---
+++ config/environments/development.rb
@@ -0,0 +1,60 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded any time
+  # it changes. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  config.active_storage.service = :local
+
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  config.active_support.deprecation = :log
+
+  config.active_support.disallowed_deprecation = :raise
+
+  config.active_support.disallowed_deprecation_warnings = []
+
+  config.active_record.migration_error = :page_load
+
+  config.active_record.verbose_query_logs = true
+
+  config.assets.debug = true
+
+
+  config.assets.quiet = true
+
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+end
